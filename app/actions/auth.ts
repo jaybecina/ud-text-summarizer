2e4161305a61de7254228a413dbd808c85bb8263
jay becina <jaybecina@gmail.com>
2024-12-09 01:58:17 +0800

    [WIP] signin function and integration

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -47,7 +47,7 @@ export async function signin(formData: SignInFormData) {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
-      maxAge: 60 * 60 * 24 * 7, // 1 week
+      maxAge: 60 * 60 * 24, // 1 day
     });
 
     return { success: true };
